65e2dcc1b05e68cd674d2f4fb9a3596550b98a79
IsaacJ <isaacjdeveloper@gmail.com>
2026-07-29 18:06:38 -0400

    formatted files, tilePictureIds, rotation and if-flipped all decided by seed now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,51 +11,66 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 export class AppComponent {
   title = 'tileset-viewer';
   zoomLevel = 0.25;
-  adjustedPxSize = TilePxSize * this.zoomLevel
-  util = new utilConstants()
-  tileGen:number[] = []
-  constructor(){
-    this.constructTiles()
+  adjustedPxSize = TilePxSize * this.zoomLevel;
+  util = new utilConstants();
+  tileGen: number[] = [];
+  constructor() {
+    this.constructTiles();
   }
-  getTileHeight():number{
-    return Math.floor(this.util.getScreenSizePercentage(100, false)/this.adjustedPxSize)+1;
+  getTileHeight(): number {
+    return (
+      Math.floor(
+        this.util.getScreenSizePercentage(100, false) / this.adjustedPxSize,
+      ) + 1
+    );
   }
 
-  getTileWidth():number{
-    return Math.floor(this.util.getScreenSizePercentage(100)/this.adjustedPxSize)+1;
+  getTileWidth(): number {
+    return (
+      Math.floor(this.util.getScreenSizePercentage(100) / this.adjustedPxSize) +
+      1
+    );
   }
 
-  constructTiles(){
-    let newTiles:Array<number> = []
-    for (let i = 0; i < this.getTileHeight()*this.getTileWidth(); i++) {
-        newTiles.push(0)      
+  tileWidth = this.getTileWidth();
+  tileHeight = this.getTileHeight();
+  
+  constructTiles() {
+    let newTiles: Array<number> = [];
+    for (let i = 0; i < this.tileWidth * this.tileHeight; i++) {
+      newTiles.push(0);
     }
     this.tileGen = newTiles;
   }
 
-  titleDisplayed = true
-  lastClicked:number = -1
-  doubleClickTime = 500
-  showingTiles = true
+  currentDate = Date.now()
+  randomSeed = this.currentDate;
+  
+  titleDisplayed = true;
+  lastClicked: number = -1;
+  doubleClickTime = 500;
+  showingTiles = true;
   @HostListener('document:mousedown', ['$event'])
   handleClickEvent(e: Event) {
     e.preventDefault();
-    if(this.titleDisplayed){
+    if (this.titleDisplayed) {
       this.titleDisplayed = false;
       this.lastClicked = Date.now();
       return;
     }
-    if(this.lastClicked + this.doubleClickTime > Date.now()){
+    if (this.lastClicked + this.doubleClickTime > Date.now()) {
       this.constructTiles();
       this.reloadTiles();
-      this.lastClicked = -1
+      this.lastClicked = -1;
     } else {
       this.lastClicked = Date.now();
     }
   }
 
-  reloadTiles(){
-    this.showingTiles = false
-    setTimeout(() => {this.showingTiles = true})
+  reloadTiles() {
+    this.showingTiles = false;
+    setTimeout(() => {
+      this.showingTiles = true;
+    });
   }
 }
--- src/app/tile-component/tile-component.component.ts
+++ src/app/tile-component/tile-component.component.ts
@@ -1,29 +1,74 @@
-import { Component, Input } from '@angular/core';
-import { TilesetSrc, TilesetCols, TilePxSize, TilesetPxHeight, TilesetPxWidth, TilesetOptions, utilConstants } from '../constants/constants.component';
+import { Component, input, Input } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import {
+  TilesetSrc,
+  TilesetCols,
+  TilePxSize,
+  TilesetPxHeight,
+  TilesetPxWidth,
+  TilesetOptions,
+  utilConstants,
+} from '../constants/constants.component';
 @Component({
   selector: 'tile-component',
   imports: [],
   templateUrl: './tile-component.component.html',
-  styleUrl: './tile-component.component.scss'
+  styleUrl: './tile-component.component.scss',
 })
-export class TileComponent {
-  @Input() zoomLevel = 1 //set by app component
-  
-  tilePxSize = TilePxSize
-  tilesetPxWidth = TilesetPxWidth
-  tilesetPxHeight = TilesetPxHeight
-  tilesetSrc = TilesetSrc
+export class TileComponent implements OnInit {
+  @Input() tileId!: number; //set by app component
+  @Input() gridX!: number; //set by app component
+  @Input() gridY!: number; //set by app component
+  @Input() zoomLevel!: number; //set by app component
+  @Input() seedVal!: number; //set by app component
+
+  tilePxSize = TilePxSize;
+  tilesetPxWidth = TilesetPxWidth;
+  tilesetPxHeight = TilesetPxHeight;
+  tilesetSrc = TilesetSrc;
   util = new utilConstants();
 
-  tileId = this.util.getRandomFromAvailableTiles()
-  tileIdX = ((this.tileId) % TilesetCols)
-  startPxX = this.tileIdX * TilePxSize * -1
-  tileIdY = (Math.floor(this.tileId / TilesetCols))
-  startPxY = this.tileIdY * TilePxSize * -1;
-  direction = this.util.getRandomDirection();
-  flipped = Math.random() < 0.5 ? "" : "flipped"
+  tileIdX!: number;
+  tileIdY!: number;
 
-  constructor(){
-    console.log(this.tilesetSrc)
+  hashVal!: number;
+  tilePictureId!: number;
+  tilePictureIdX!: number;
+  startPxX!: number;
+  tilePictureIdY!: number;
+  startPxY!: number;
+  direction!: string;
+  flipped!: string;
+
+  ngOnInit(): void {
+    this.tileIdX = this.tileId % this.gridX;
+    this.tileIdY = Math.floor(this.tileId / this.gridX);
+
+    this.hashVal = this.util.createHash(
+      this.seedVal || 0,
+      this.tileIdX,
+      this.tileIdY,
+    );
+
+    this.tilePictureId = this.util.getTileById(this.hashVal);
+    
+    this.tilePictureIdX = this.tilePictureId % TilesetCols;
+    this.startPxX = this.tilePictureIdX * TilePxSize * -1;
+    this.tilePictureIdY = Math.floor(this.tilePictureId / TilesetCols);
+    this.startPxY = this.tilePictureIdY * TilePxSize * -1;
+
+    this.direction = this.util.getRotationByHash(this.hashVal); //this.util.getRandomDirection();
+    this.flipped =((this.hashVal >> 10) & 1) === 1 ? '' : 'flipped';
+
+    console.log(
+      this.tileIdX,
+      ' ',
+      this.tileIdY,
+      ' ',
+      this.tilePictureId,
+      ' ',
+      this.hashVal,
+    );
   }
 }
